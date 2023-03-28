a6ee21d57fc0eeec7324b791baa069773c58244e
vanessaokumu <okumuvanessa@gmail.com>
2023-03-28 03:34:04 -0700

    install nginx

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,23 +1,20 @@
 # Install nginx with puppet
 package { 'nginx':
-  ensure   => installed,
-  provider => 'apt',
+  ensure     => 'installed',
 }
 
-file { 'Hello World':
-  path    => '/var/www/html/index.nginx-debian.html',
-  content => 'Hello World',
+file { '/var/www/html/index.html':
+  content => 'Holberton School',
 }
 
-file_line { 'Hello World':
-  path  => '/etc/nginx/sites-available/default',
-  after => 'server_name _;',
-  line  => '\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+file_line { 'aaaaa':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
-exec { 'service':
-  command  => 'service nginx start',
-  provider => 'shell',
-  user     => 'root',
-  path     => '/usr/sbin/service',
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
