ac44bf935da38a7425ea2025e21ddbc6de06866b
vanessaokumu <okumuvanessa@gmail.com>
2023-09-25 05:06:35 -0700

    gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+Python script that, using this REST API, for a given employee ID, 
+returns information about his/her TODO list progress.
+"""
+
+import requests
+
+def get_employee_todo_progress(employee_id):
+    # Define the API endpoint URL
+    base_url = "https://jsonplaceholder.typicode.com"
+    todo_url = f"{base_url}/todos"
+    user_url = f"{base_url}/users/{employee_id}"
+
+    try:
+        # Fetch the employee's TODO list and user details
+        todo_response = requests.get(todo_url, params={"userId": employee_id})
+        user_response = requests.get(user_url)
+
+        # Check if the requests were successful
+        if todo_response.status_code != 200 or user_response.status_code != 200:
+            print("Failed to retrieve data. Please check the employee ID.")
+            return
+
+        # Parse JSON data from the responses
+        todo_data = todo_response.json()
+        user_data = user_response.json()
+
+        # Extract user details
+        employee_name = user_data.get("name")
+
+        # Calculate TODO list progress
+        total_tasks = len(todo_data)
+        completed_tasks = sum(1 for task in todo_data if task["completed"])
+
+        # Display the progress information
+        print(f"Employee {employee_name} is done with tasks ({completed_tasks}/{total_tasks}):")
+        print(f"{employee_name}: {completed_tasks} tasks completed out of {total_tasks}")
+
+        # Display completed task titles
+        for task in todo_data:
+            if task["completed"]:
+                print(f"\t{task['title']}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    employee_id = int(input("Enter the employee ID: "))
+    get_employee_todo_progress(employee_id)
