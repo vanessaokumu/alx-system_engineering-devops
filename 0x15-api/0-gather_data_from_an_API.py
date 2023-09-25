5d530677e30425420bd91c1af881175b17fb2d1b
vanessaokumu <okumuvanessa@gmail.com>
2023-09-25 05:44:04 -0700

    update

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Python script that, using this REST API, for a given employee ID, 
+Python script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress.
 """
 
@@ -20,7 +20,8 @@ def get_employee_todo_progress(employee_id):
         user_response = requests.get(user_url)
 
         # Check if the requests were successful
-        if todo_response.status_code != 200 or user_response.status_code != 200:
+        if todo_response.status_code != 200 or
+        user_response.status_code != 200:
             print("Failed to retrieve data. Please check the employee ID.")
             return
 
@@ -36,9 +37,10 @@ def get_employee_todo_progress(employee_id):
         completed_tasks = sum(1 for task in todo_data if task["completed"])
 
         # Display the progress information
-        print(f"Employee {employee_name} is done with tasks ({completed_tasks}/{total_tasks}):"
+        print(f"Employee {employee_name} is done with tasks
+                ({completed_tasks}/{total_tasks}):"
               .format(employee_name, completed_tasks, total_tasks))
-        
+
         # Display completed task titles
         for task in todo_data:
             if task["completed"]:
