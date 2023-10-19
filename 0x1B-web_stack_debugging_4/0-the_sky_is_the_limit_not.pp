5124c457f3745035bb6f62e2234f06ad682bfb1d
vanessaokumu <okumuvanessa@gmail.com>
2023-10-19 12:33:16 -0700

    debugging

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# let’s fix our stack so that we get to 0 requests failed
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
+}
+
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
